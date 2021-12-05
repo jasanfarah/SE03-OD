c42f0925bf3c1aa1cb8040ac943ecc51d4709f11
jasan <jasanfarah@gmail.com>
2021-12-05 22:18:10 +0100

    update

--- backend/app.py
+++ backend/app.py
@@ -18,31 +18,30 @@ def start():
     return '<h1> Welcome <h1> <br> <a href="/person">Person</a> <br> <a href="/persons">Persons</a>'
 @app.route('/person',methods=['POST'])
 def person():
-    if request.method == "POST":
-        firstName = request.form['firstname']
-        lastName = request.form['lastname']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO person_table(Firstname, Lastname) VALUES (%s, %s)", (firstName, lastName))
-        mysql.connection.commit()
-        cur.close()
+    firstName = request.form['firstname']
+    lastName = request.form['lastname']
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO person_table(Firstname, Lastname) VALUES (%s, %s)", (firstName, lastName))
+    mysql.connection.commit()
+    cur.close()
     return redirect('https://localhost/select.html',code=200)
 
 @app.route('/persons', methods=['GET'])
 def persons():
-        if request.method == "GET":
-            cur = mysql.connection.cursor()
-            cur.execute("SELECT * FROM person_table")
-            dbData = cur.fetchall()
-            persons =[ ]
-            for row in dbData:
-                obj= {
-                    "Firstname": row['Firstname'],
-                    "PersonID" : row['PersonID'],
-                    "Lastname" : row['Lastname']
-                }
-                persons.append(obj)
-            data= jsonify(persons)
-        return data
+    
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM person_table")
+    dbData = cur.fetchall()
+    persons =[ ]
+    for row in dbData:
+        obj= {
+        "Firstname": row['Firstname'],
+        "PersonID" : row['PersonID'],
+        "Lastname" : row['Lastname']
+        }
+    persons.append(obj)
+    data= jsonify(persons)
+    return data
 
 
 if __name__ == '__main__':
